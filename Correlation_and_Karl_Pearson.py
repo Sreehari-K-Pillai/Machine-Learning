9a2fb3faa9f0a305c9f293f7217b843166fb59cc
Sreehari-K-Pillai <53552488+Sreehari-K-Pillai@users.noreply.github.com>
2019-08-09 23:50:51 +0530

    Update Correlation_and_Karl_Pearson.py

--- Correlation_and_Karl_Pearson.py
+++ Correlation_and_Karl_Pearson.py
@@ -1,15 +1,10 @@
 import numpy as np
-
 import pandas as pd
-
 x = [15,12,8,8,7,7,7,6,5,3]
 y = [10,25,17,11,13,17,20,13,9,15]
-
 x = pd.Series(x)
 print(x)
-
 y = pd.Series(y)
 print(y)
-
 r = x.cov(y) / (x.std() * y.std())
 print("%.3f"%(r))
