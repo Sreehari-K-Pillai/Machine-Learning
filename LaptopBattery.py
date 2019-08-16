0630e40afe4d7b45e98e8d24c94c06905e007722
Sreehari-K-Pillai <53552488+Sreehari-K-Pillai@users.noreply.github.com>
2019-08-16 22:06:13 +0530

    Update LaptopBattery.py

--- LaptopBattery.py
+++ LaptopBattery.py
@@ -1,22 +1,5 @@
-import pandas as pd
-from urllib.request import urlopen
-from io import StringIO
-import csv
-data=urlopen('https://s3.amazonaws.com/hr-testcases/399/assets/trainingdata.txt').read().decode('ascii','ignore')
-datafile=StringIO(data)
-csvReader=csv.reader(datafile)
-a=[]
-b=[]
-for i in csvReader:
-    a.append(i[0])
-    b.append(i[1])
-time_charged=[float(i) for i in a]
-time_lasted=[float(i) for i in b]
-x = pd.Series(time_charged)
-y = pd.Series(time_lasted)
-r = x.cov(y)/(x.std()*y.std())
-beta1=(r*y.std())/x.std()
-beta0=y.mean()-(beta1*x.mean())
-m=float(input(""))
-y=(beta1*m)+beta0
-print("%d" %y)
+x=float(input())
+if x >= 4.00:
+    print(8.00)
+else:
+    print('%.2f'%(2*x))
